69f976d5f683b87259eb06e569511744683f0da6
denver <dnaicker@gmail.com>
2017-02-12 13:22:48 +0200

    highlight paragraph on text selection

--- libs/custom.js
+++ libs/custom.js
@@ -3,4 +3,25 @@
     $( ".header" ).load( "navbar.html", function() {
 		//events for bootstrap
 	});
+
+    //for every paragrap highlight
+    $('.paragraph').on('click', function(event) {
+    	console.log('click');
+    	//split screen
+    	//popup side dialog with box for editting and overflow scrolling for 
+    	//all annotations
+
+    	//remove previous selection
+    	$('.paragraph').css({'background-color': 'white', 'text-decoration': 'none'});
+    	//highlight current selection
+    	$(this).css({'background-color': 'yellow', 'text-decoration': 'underline'});
+    });
+
+    $('html').on('click', function(event) {
+		//remove previous selections    	
+    	if ( !$(event.target).hasClass('paragraph')) {
+        	$('.paragraph').css({'background-color': 'white', 'text-decoration': 'none'});
+    	}
+    	
+    });
   });
